e6dd79f59204b47a86214a5e515e1b3511077f1c
lr <lrmail>
2022-03-05 23:31:27 +0100

    Add String Property for testing purposes

--- tests/UnitTests/SyncWpfFormTests/SampleBackingObject.cs
+++ tests/UnitTests/SyncWpfFormTests/SampleBackingObject.cs
@@ -3,4 +3,5 @@ namespace PCC.Datastructures.CSharp.WpfForm.UnitTests.SyncWpfFormTests;
 public class SampleBackingObject
 {
     public int SomeInteger { get; set; } = 42;
+    public string SomeString { get; set; } = "nobody";      
 }
