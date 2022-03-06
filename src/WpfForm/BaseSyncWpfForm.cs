d6ac777c6664bca94e4b9048b9c8d021c532fe65
lr <lrmail>
2022-03-07 00:13:17 +0100

    Listen for changes of properties that implement INotifyPropertyChanged

--- src/WpfForm/BaseSyncWpfForm.cs
+++ src/WpfForm/BaseSyncWpfForm.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using PCC.Libs.Nulls;
 
@@ -6,13 +7,32 @@ namespace PCC.Datastructures.CSharp.WpfForm;
 
 public abstract class BaseSyncWpfForm : INotifyPropertyChanged
 {
+    private readonly Dictionary<PropertyInfo, Tuple<INotifyPropertyChanged, PropertyChangedEventHandler>>
+        _registeredChangeListeners =
+            new Dictionary<PropertyInfo, Tuple<INotifyPropertyChanged, PropertyChangedEventHandler>>();
     private readonly Dictionary<string, object> _unsavedValues = new Dictionary<string, object>();
 
     protected BaseSyncWpfForm(object store)
     {
         Store = store;
+        ListenForChangesInStore();
     }
-    
+
+    private void ListenForChangesInStore()
+    {
+        var propertiesThatInformChanges = Store.GetType().GetProperties().Where(p => p.PropertyType.GetInterface(nameof(INotifyPropertyChanged)) != null);
+        foreach (var propertyThatInformChanges in propertiesThatInformChanges)
+        {
+            if (!(propertyThatInformChanges.GetValue(Store) is INotifyPropertyChanged property)) continue;
+            var handler = new PropertyChangedEventHandler((o, e) =>
+            {
+                FirePropertyChanged(propertyThatInformChanges.Name);
+            });
+            property.PropertyChanged += handler;
+            _registeredChangeListeners.Add(propertyThatInformChanges, Tuple.Create(property, handler));
+        }
+    }
+
     public event PropertyChangedEventHandler? PropertyChanged;
     
     public bool IsDirty => _unsavedValues.Any();
