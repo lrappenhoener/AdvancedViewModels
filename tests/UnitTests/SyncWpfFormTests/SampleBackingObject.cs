7c1a332c846d54cb0d6ac65524b2b8cd394c66aa
lr <lrmail>
2022-03-06 23:46:40 +0100

    Introduce SomeComplex Property

--- tests/UnitTests/SyncWpfFormTests/SampleBackingObject.cs
+++ tests/UnitTests/SyncWpfFormTests/SampleBackingObject.cs
@@ -2,6 +2,19 @@ namespace PCC.Datastructures.CSharp.WpfForm.UnitTests.SyncWpfFormTests;
 
 public class SampleBackingObject
 {
+    private static int _depth = 0;
+    public SampleBackingObject()
+    {
+        _depth++;
+        if (_depth > 2) return;
+        SomeComplex = new SampleBaseSyncWpfForm(new SampleBackingObject
+        {
+            SomeInteger = 1977,
+            SomeString = "Birthday"
+        });
+    }
+    
     public int SomeInteger { get; set; } = 42;
-    public string SomeString { get; set; } = "nobody";      
+    public string SomeString { get; set; } = "nobody";
+    public SampleBaseSyncWpfForm SomeComplex { get; set; } 
 }
