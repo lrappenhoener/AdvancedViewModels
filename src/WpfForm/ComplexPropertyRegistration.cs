f02c38f673d1136c9cb8736e5ffc56694ae57ae5
lr <lrmail>
2022-03-07 18:11:51 +0100

    Use IComplexProperty Target

--- src/WpfForm/ComplexPropertyRegistration.cs
+++ src/WpfForm/ComplexPropertyRegistration.cs
@@ -4,7 +4,7 @@ namespace PCC.Datastructures.CSharp.WpfForm;
 
 internal class ComplexPropertyRegistration
 {
-    public ComplexPropertyRegistration(string propertyName, PropertyChangedEventHandler handler, INotifyPropertyChanged target)
+    public ComplexPropertyRegistration(string propertyName, PropertyChangedEventHandler handler, IComplexProperty target)
     {
         PropertyName = propertyName;
         Handler = handler;
@@ -13,5 +13,5 @@ internal class ComplexPropertyRegistration
 
     internal string PropertyName { get; private set; }
     internal PropertyChangedEventHandler Handler { get; private set; }
-    internal INotifyPropertyChanged Target { get; private set; }
+    internal IComplexProperty Target { get; private set; }
 }
