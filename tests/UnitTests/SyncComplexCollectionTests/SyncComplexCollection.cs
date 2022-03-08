9dad75e3fba44e660a82475fe78b6585bdf3c43d
lr <lrmail>
2022-03-08 11:38:35 +0100

    Introduce Class "SyncComplexCollection<T>"

---
+++ tests/UnitTests/SyncComplexCollectionTests/SyncComplexCollection.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+
+namespace PCC.Datastructures.CSharp.WpfForm.UnitTests.SyncComplexCollectionTests;
+
+public class SyncComplexCollection<T> : ISyncComplexCollection<T>
+{
+    public event NotifyCollectionChangedEventHandler? CollectionChanged;
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public bool IsDirty { get; }
+    public void AcceptChanges()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void RejectChanges()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Add(T item)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Clear()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool Contains(T item)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool Remove(T item)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public int Count { get; }
+    public bool IsReadOnly { get; }
+    public int IndexOf(T item)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Insert(int index, T item)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void RemoveAt(int index)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public T this[int index]
+    {
+        get => throw new System.NotImplementedException();
+        set => throw new System.NotImplementedException();
+    }
+}
