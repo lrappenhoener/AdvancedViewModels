90aa4af205e71cd69cca76d88cb89bbe6b37b3f2
lr <lrmail>
2022-03-08 12:22:25 +0100

    Add more sut factories for testing purposes

--- tests/UnitTests/ISyncComplexCollectionTests/SyncComplexCollectionTests.cs
+++ tests/UnitTests/ISyncComplexCollectionTests/SyncComplexCollectionTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using PCC.Datastructures.CSharp.WpfForm.UnitTests.Common;
 
 namespace PCC.Datastructures.CSharp.WpfForm.UnitTests.SyncComplexCollectionTests;
@@ -9,8 +10,24 @@ public class SyncComplexCollectionTests : Tests
         return new SyncComplexCollection<SampleBaseSyncWpfForm>();
     }
 
+    protected override SyncComplexCollection<SampleBaseSyncWpfForm> CreateSut(IEnumerable<SampleBaseSyncWpfForm> elements)
+    {
+        return new SyncComplexCollection<SampleBaseSyncWpfForm>(elements);
+    }
+
     protected override SampleBaseSyncWpfForm CreateElement()
     {
         return new SampleBaseSyncWpfForm(new SampleBackingObject(0), 0);
+    }
+
+    protected override IEnumerable<SampleBaseSyncWpfForm> CreateElements(int count)
+    {
+        var elements = new List<SampleBaseSyncWpfForm>();
+        for (int i = 0; i < count; i++)
+        {
+            elements.Add(CreateElement());
+        }
+
+        return elements;
     }
 }
--- tests/UnitTests/ISyncComplexCollectionTests/Tests.cs
+++ tests/UnitTests/ISyncComplexCollectionTests/Tests.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using PCC.Datastructures.CSharp.WpfForm.UnitTests.Common;
 using PCC.Datastructures.CSharp.WpfForm.UnitTests.SyncWpfFormTests;
@@ -8,7 +10,9 @@ namespace PCC.Datastructures.CSharp.WpfForm.UnitTests.SyncComplexCollectionTests
 public abstract class Tests
 {
     protected abstract SyncComplexCollection<SampleBaseSyncWpfForm> CreateSut();
+    protected abstract SyncComplexCollection<SampleBaseSyncWpfForm> CreateSut(IEnumerable<SampleBaseSyncWpfForm> elements);
     protected abstract SampleBaseSyncWpfForm CreateElement();
+    protected abstract IEnumerable<SampleBaseSyncWpfForm> CreateElements(int count);
 
     [Fact]
     public void NotDirty_Initially()
