4cc8012339bdc02ac59ae698abb1e1548ac621e8
lr <lrmail>
2022-03-08 12:41:34 +0100

    Add test "Returns_Correct_Element_After_Replacing"

--- tests/UnitTests/ISyncComplexCollectionTests/Tests.cs
+++ tests/UnitTests/ISyncComplexCollectionTests/Tests.cs
@@ -56,4 +56,17 @@ public abstract class Tests
 
         returned.Should().Be(expected);
     }
+    
+    [Fact]
+    public void Returns_Correct_Element_After_Replacing()
+    {
+        var elements = CreateElements(10);
+        var sut = CreateSut(elements);
+        var expected = CreateElement();
+        sut[3] = expected;
+
+        var returned = sut.ElementAt(3);
+
+        returned.Should().Be(expected);
+    }
 }
