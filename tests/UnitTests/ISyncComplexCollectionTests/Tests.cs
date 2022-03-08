95719cfa3865dfcc61023d8a80048589c9900de9
lr <lrmail>
2022-03-08 12:04:58 +0100

    Add test "NotDirty_Initially"

--- tests/UnitTests/ISyncComplexCollectionTests/Tests.cs
+++ tests/UnitTests/ISyncComplexCollectionTests/Tests.cs
@@ -1,5 +1,7 @@
+using FluentAssertions;
 using PCC.Datastructures.CSharp.WpfForm.UnitTests.Common;
 using PCC.Datastructures.CSharp.WpfForm.UnitTests.SyncWpfFormTests;
+using Xunit;
 
 namespace PCC.Datastructures.CSharp.WpfForm.UnitTests.SyncComplexCollectionTests;
 
@@ -7,4 +9,12 @@ public abstract class Tests
 {
     protected abstract SyncComplexCollection<SampleBaseSyncWpfForm> CreateSut();
     protected abstract SampleBaseSyncWpfForm CreateElement();
+
+    [Fact]
+    public void NotDirty_Initially()
+    {
+        var sut = CreateSut();
+
+        sut.IsDirty.Should().BeFalse();
+    }
 }
