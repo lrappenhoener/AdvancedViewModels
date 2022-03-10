ea65114072e1cf7a8752e58d155470a7a030e3a2
lr <lrmail>
2022-03-10 10:15:53 +0100

    Remove IStore Interface

--- src/WpfForm/IStore.cs
+++
@@ -1,9 +0,0 @@
-using PCC.Libs.Nulls;
-
-namespace PCC.Datastructures.CSharp.WpfForm;
-
-public interface IStore
-{
-    void SetValue(string propertyName, object value);
-    Maybe<object> GetValue(string propertyName);
-}
