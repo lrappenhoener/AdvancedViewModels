ab1924a3eb165ba54aeb2252d0d090c0a4ba924b
lr <lrmail>
2022-03-20 00:02:57 +0100

    Remove implementation for INotifyDataErrorInfo from ITrackChanges.cs

--- src/BaseViewModel/ITrackChanges.cs
+++ src/BaseViewModel/ITrackChanges.cs
@@ -2,7 +2,7 @@ using System.ComponentModel;
 
 namespace PCC.Datastructures.CSharp.BaseViewModel;
 
-public interface ITrackChanges : INotifyPropertyChanged, INotifyDataErrorInfo
+public interface ITrackChanges : INotifyPropertyChanged
 {
     bool IsDirty { get; }
     void AcceptChanges();
