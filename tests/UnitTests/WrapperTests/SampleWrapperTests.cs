39a50f7a53a4e41e086cff0867cb84c7a941b1bc
lr <lrmail>
2022-07-01 13:22:38 +0200

    Add test Model_Set_Successful

---
+++ tests/UnitTests/WrapperTests/SampleWrapperTests.cs
@@ -0,0 +1,18 @@
+using FluentAssertions;
+using PCC.Libraries.AdvancedViewModels.UnitTests.Common;
+using Xunit;
+
+namespace PCC.Libraries.AdvancedViewModels.UnitTests.WrapperTests;
+
+public class SampleWrapperTests
+{
+    [Fact]
+    public void Model_Set_Successful()
+    {
+        var model = new SampleWrapper();
+        
+        var wrapper = new Wrapper<SampleWrapper>(model);
+
+        wrapper.Model.Should().Be(model);
+    }
+}
